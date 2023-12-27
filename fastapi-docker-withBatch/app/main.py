7b2bfb736ef7d6ffacd84b7ac8f38475795b67a6
ebrukirikkayis <ebrukirikkayis@gmail.com>
2023-12-27 20:07:41 +0300

    upload

---
+++ fastapi-docker-withBatch/app/main.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 27 19:04:52 2023
+
+@author: EBRU
+"""
+
+import pickle
+import numpy as np
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel, conlist
+
+
+app = FastAPI(title="Predicting Wine Class with batching")
+
+class Wine(BaseModel):
+    batches: List[conlist(item_type=float, min_length=13, max_length=13)]
+
+
+@app.on_event("startup")
+def load_clf():
+    with open("./app/wine.pkl", "rb") as file:
+        global clf
+        clf = pickle.load(file)
+
+
+@app.get("/")
+def home():
+    return "Congratulations! Your API is working as expected. This new version allows for batching. Now head over to http://localhost:81/docs"
+
+
+@app.post("/predict")
+def predict(wine: Wine):
+    batches = wine.batches
+    np_batches = np.array(batches)
+    pred = clf.predict(np_batches).tolist()
+    return {"Prediction": pred}
